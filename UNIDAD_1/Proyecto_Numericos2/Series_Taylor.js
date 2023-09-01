d9e7ec9ed7ca93da0f17f036d5b7dc6d4333aee4
FostDull <125672490+FostDull@users.noreply.github.com>
2023-09-01 09:37:39 -0500

    actualización pagina web

---
+++ UNIDAD_1/Proyecto_Numericos2/Series_Taylor.js
@@ -0,0 +1,41 @@
+document.querySelector('.Unidades').addEventListener('click', function() {
+    // El código que estaba dentro del evento DOMContentLoaded
+    // va aquí
+    document.getElementById('calculator-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const x = parseFloat(document.getElementById('x').value);
+        const n = parseInt(document.getElementById('n').value);
+
+        const result = calculateTaylorSeries(x, n);
+        document.getElementById('result').textContent = `El resultado de la serie de Taylor es: ${result}`;
+    });
+
+    function calculateTaylorSeries(x, n) {
+        let result = 0.0;
+
+        for (let i = 0; i < n; i++) {
+            result += power(x, i) / factorial(i);
+        }
+
+        return result;
+    }
+
+    function power(base, exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    function factorial(n) {
+        if (n === 0 || n === 1) {
+            return 1;
+        }
+
+        let result = 1;
+        for (let i = 2; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+});
+ 
